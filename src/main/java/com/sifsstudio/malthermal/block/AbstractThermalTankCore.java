4b2b1a91816eba6865d4326933588dafce891c4e
switefaster <switefaster@gmail.com>
2022-07-09 21:47:10 +0800

    refactor(import): optimize imports

--- src/main/java/com/sifsstudio/malthermal/block/AbstractThermalTankCore.java
+++ src/main/java/com/sifsstudio/malthermal/block/AbstractThermalTankCore.java
@@ -12,7 +12,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
